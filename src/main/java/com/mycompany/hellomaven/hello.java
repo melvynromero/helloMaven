9a71569401b2fe9ec3307c6bee7eba1035cf4946
Romero, Melvyn <melvyn.romero@one.verizon.com>
2016-07-05 21:43:23 -0400

    New Proyecto for testing Maven and Jenkinsfile

---
+++ src/main/java/com/mycompany/hellomaven/hello.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hellomaven;
+
+
+/**
+ *
+ * @author romeme3
+ */
+public class hello {
+        public static void main (String[] args ) {
+
+	  
+	   System.out.println("Hello Java " );    
+        }
+    }
